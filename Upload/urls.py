d7d70946be210b6572e405d48f045ebc039d2736
blastxiaol <blastxiaol@gmail.com>
2023-01-14 20:12:51 +0800

    upload

--- Upload/urls.py
+++ Upload/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.upload, name='upload')
+    path(r'', views.upload, name='upload'),
+    path(r'batch_upload', views.batch_upload, name='batch_upload')
 ]
--- Upload/views.py
+++ Upload/views.py
@@ -1,111 +1,65 @@
 # from django.shortcuts import render
-from django.http import JsonResponse
 import os
+from django.http import JsonResponse
 import json
-import numpy as np
-import cv2
-from .models import Scenes, Frames, Instances
-from .utils import loc_pc2img
+from .utils import upload2database
 
 # Create your views here.
 def upload(request):
     # ################# 权限判断 ################# #
     data = json.loads(request.body)
     output = {
-        'result': False
+        'result': False,
+        'information': '权限不足'
     }
     if data['authority'] != 'Manager':
          return JsonResponse(output)
+    if not os.path.exists(data['data']['filepath']):
+        output['information'] = '路径错误'
+        return JsonResponse(output)
     # ########################################### #
-    location = data['data']['location']
-    date = data['data']['date']
-    date = date.split('T')[0]
-    filepath = data['data']['filepath']
     
-
-    file = json.load(open(filepath))
-    # 上传场景数据
-    try:
-        scene = Scenes.objects.get(annotation_path=filepath)
-    except:
-        scene = Scenes()
-        scene.annotation_path = filepath
-        scene.location = location
-        scene.date = date
-        scene.save()
-        
-    scene_id = scene.id
-    group = file['group']
-    for frame_data in file['frames']:
-        timestamp = frame_data['timestamp']
-        pc_path = frame_data['pc_name']
-        img_path = frame_data['image_name']
-        img_path = os.path.join(f'/Users/apple/Desktop/test/123/test_folder/{group}', img_path.split('/')[-1])
-        try:
-            frame = Frames.objects.get(pc_path=pc_path)
-        except:
-            frame = Frames()
-            frame.scene_id = scene_id
-            frame.timestamp = timestamp
-            frame.pc_path = pc_path
-            frame.img_path = img_path
-            frame.save()
-
-        frame_id = frame.id
-        instance_list = frame_data['instance']
-        image = cv2.imread(str(frame.img_path))
-        for instance_data in instance_list:
-            category = instance_data['category']
-            if category != 'person':
-                continue
-            object_key = instance_data['id']
-            occlusion = True if instance_data['occlusion'] == 1 else False
-
-            center = instance_data['position']
-            boundingbox3d = instance_data['boundingbox3d']
-            rotation = instance_data['rotation']
-            box3d = [center['x'], center['y'], center['z'], boundingbox3d['x'], boundingbox3d['y'], boundingbox3d['z'], rotation]
-            box2d = loc_pc2img(np.array(box3d))
-            # 过滤中心不在图片上的点
-            left_up = box2d[:, 0].min(), box2d[:, 1].min()
-            right_down = box2d[:, 0].max(), box2d[:, 1].max()
-            cy, cx = (np.array(right_down) + np.array(left_up)) / 2
-            if cx < 0 or cx >= image.shape[0] or cy <0 or cy >= image.shape[1]:
-                continue
-            box2d = box2d[[6, 7, 3, 2, 5, 4, 0, 1]]
-
-            box2d = box2d.reshape(-1)
-            box2d = ','.join([str(_) for _ in box2d.tolist()])
-            box3d = ','.join([str(_) for _ in box3d])
-            # box2d = np.array([int(_) for _ in box2d.split(',')]).reshape(-1, 2) # 还原框
-            try:
-                instance = Instances.objects.get(
-                    frame_id=frame_id,
-                    object_key=object_key,
-                )
-            except:
-                instance = Instances()
-                instance.frame_id = frame_id
-                instance.object_key = object_key
-                instance.box2d = box2d
-                instance.box3d = box3d
-                instance.occlusion = occlusion
-                instance.described = False
-                instance.save()
     
-    ## 检查前后frame
-    for frame_data in file['frames']:
-        pc_path = frame_data['pc_name']
-        previous_pc_path = frame_data['previous_pc']
-        next_pc_path = frame_data['next_pc']
-        frame = Frames.objects.get(pc_path=pc_path)
-        if previous_pc_path:
-            previous_frame = Frames.objects.get(pc_path=previous_pc_path)
-            frame.prev_frame_id = previous_frame.id
-        if next_pc_path:
-            next_frame = Frames.objects.get(pc_path=next_pc_path)
-            frame.next_frame_id = next_frame.id
-        frame.save()
+    result = upload2database(data)
         
-    output['result'] = True
+    output['result'] = result
+    if result == False:
+        output['information'] = '场景已存在'
+    else:
+        output['information'] = '上传成功'
+    return JsonResponse(output)
+
+def batch_upload(request):
+    data = json.loads(request.body)
+    output = {
+        'result': False,
+        'information': '权限不足'
+    }
+    if data['authority'] != 'Manager':
+         return JsonResponse(output)
+    if not os.path.exists(data['data']['folderpath']):
+        output['information'] = '路径错误'
+        return JsonResponse(output)
+    ##########################################
+
+    filelist = os.listdir(data['data']['folderpath'])
+    valid = 0
+    invalid = 0
+    for filepath in filelist:
+        if not filepath.endswith('.json'):
+            continue
+        filepath = os.path.join(data['data']['folderpath'], filepath)
+        result = upload2database(filepath, True)
+        if result:
+            valid += 1
+        else:
+            invalid += 1
+
+
+    output['result'] = True if valid > 0 else False
+    output['information'] = f'上传{valid}条数据，已存在{invalid}条数据'
+    if output['result'] == False:
+        output['information'] = '上传失败\n' + output['information']
+    else:
+        output['information'] = '上传成功\n' + output['information']
     return JsonResponse(output)
