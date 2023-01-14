65d8857074dedbe172ed45f6f62e3cf4be226606
blastxiaol <blastxiaol@gmail.com>
2023-01-14 23:54:22 +0800

    download

--- STAnnotation/urls.py
+++ STAnnotation/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-backend = ''
+backend = 'backend/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
